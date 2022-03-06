63a546d940428559842ffca6423c31252c9c2597
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-03-06 11:40:44 +0100

    changes on db-ssl verification

--- queries_prod.js
+++ queries_prod.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production'){
     }
 }
 
-const client = new Client(clientSettings);
+const client = new Client({connectionString: process.env.DATABASE_URL});
 
 
 client.connect();
